880cb7b6ebfc9b7cdacdd5128ded0d091cb28c7f
nishadabhay18 <abhayn927@gmail.com>
2026-05-07 11:00:17 +0530

    initial commit

--- BackEnd/src/controllers/ai.controller.js
+++ BackEnd/src/controllers/ai.controller.js
@@ -1,17 +1,44 @@
-const aiService = require("../services/ai.service")
+// const aiService = require("../services/ai.service")
 
 
-module.exports.getReview = async (req, res) => {
+// module.exports.getReview = async (req, res) => {
 
-    const code = req.body.code;
+//     const code = req.body.code;
 
-    if (!code) {
-        return res.status(400).send("Prompt is required");
+//     if (!code) {
+//         return res.status(400).send("Prompt is required");
+//     }
+
+//     const response = await aiService(code);
+
+
+//     res.send(response);
+
+// }
+
+const aiService = require("../services/ai.service");
+
+const getReview = async (req, res) => {
+    try {
+        
+        // const code = req.query.prompt;
+
+        const code = req.body.code;
+
+        // const { code } = req.body;
+
+        if (!code) {
+            return res.status(400).send("Prompt is required");
+        }
+
+        const response = await aiService(code);
+
+        res.send(response); 
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
     }
+};
 
-    const response = await aiService(code);
-
-
-    res.send(response);
-
-}
+module.exports = { getReview };
